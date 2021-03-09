dceea1ce04d9a812a6dbef5550d028a46c7dd40c
Dmitry Matyushkin <dmatushkin@gmail.com>
2021-03-09 19:48:48 +0400

    Initial Commit

---
+++ TranslationToolSet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslationToolSet
+//
+//  Created by Dmitry Matyushkin on 3/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
